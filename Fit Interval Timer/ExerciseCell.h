98648a45db936d19337f278ba59d5c92dc3945db
Lena Hsieh <lena_hsieh@hotmail.com>
2014-03-10 22:55:54 -0400

    before adding in core data

---
+++ Fit Interval Timer/ExerciseCell.h
@@ -0,0 +1,13 @@
+//
+//  ExerciseCell.h
+//  Fit Interval Timer
+//
+//  Created by Lena Hsieh on 3/5/2014.
+//  Copyright (c) 2014 hsieh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExerciseCell : UITableViewCell
+
+@end
